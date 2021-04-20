2fdb9ab1b68e00e8c9a5b985a31c56d8a17f2633
Manu <emmanuel.colussi@hpe.com>
2021-04-20 08:47:42 +0200

    Add files via upload

--- k8sdeploy-scripts/kubeadm-install.sh
+++ k8sdeploy-scripts/kubeadm-install.sh
@@ -3,6 +3,12 @@
 K8_VERSION=${1}
 
 set -e
+/usr/sbin/setenforce 0
+sed -i --follow-symlinks 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
+systemctl stop firewalld
+systemctl disable firewalld.service
+/usr/sbin/swapoff -a
+sed -i '/swap/ s/^/# /' /etc/fstab 
 /bin/cp /tmp/k8sdeploy-scripts/kubernetes.repo  /etc/yum.repos.d
 yum install -y kubelet-${K8_VERSION} kubeadm-${K8_VERSION} kubectl-${K8_VERSION} 
 /usr/bin/systemctl enable kubelet.service
